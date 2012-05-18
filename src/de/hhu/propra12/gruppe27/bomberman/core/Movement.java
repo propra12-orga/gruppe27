d12ba9602350cddc3b01a2aac5b7696437a9fa3d
QRush <kurosch123@yahoo.de>
2012-05-18 12:44:26 +0200

    Neue Klasse "Movement.java" momentan ohne Funktion, da FPS("graphics") und KeyListener("gui" -> muss auf das Spielfeld fokusiert werden.) fehlen.

---
+++ src/de/hhu/propra12/gruppe27/bomberman/core/Movement.java
@@ -0,0 +1,22 @@
+package de.hhu.propra12.gruppe27.bomberman.core;
+
+/*
+//Es fehlt nocht ein KeyListener. Er sollte auf das Spielfeld fokusiert sein, dann kann ich diesen hier einbinden.
+//Hier wird im Grunde bei jeder Tasteneingabe abgefragt, ob das genannte Feld frei ist. Wenn es frei ist, wird die variable posx oder posy verändert.
+//Da das Spielfeld sowieso ständig neu gezeichnet wird sollte das verändern der Positionsvariable reichen.
+
+public class Movement {
+	
+	public static void move(char taste){
+		
+		switch (taste){
+		case 'W': if(laxbr[Player.posx][Player.posy-1] == true){Player.posy--;} break;
+		case 'A': if(laxbr[Player.posx-1][Player.posy] == true){Player.posx--;} break;
+		case 'S': if(laxbr[Player.posx][Player.posy+1] == true){Player.posy++;} break;
+		case 'D': if(laxbr[Player.posx+1][Player.posy] == true){Player.posx++;} break;
+		default: break;
+		}
+	}
+}
+
+*/
