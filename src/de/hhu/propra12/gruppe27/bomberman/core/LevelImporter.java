990ed1b2d3afa24fa90ce7ab3a767be1bbfffa4f
Erik Berkholz <erik.berkholz@uni-duesseldorf.de>
2012-06-09 13:25:09 +0200

    level einlesen by QRush

--- src/de/hhu/propra12/gruppe27/bomberman/core/LevelImporter.java
+++ src/de/hhu/propra12/gruppe27/bomberman/core/LevelImporter.java
@@ -3,8 +3,14 @@ package de.hhu.propra12.gruppe27.bomberman.core;
 import java.io.*;
 import java.util.Properties;
 
-public class LevelImporter {
-	public static void importLevel() throws IOException{	//Fürs erste Standaloneprojekt - FileInputStream könnte bei nicht Vorhanden sein der Datei Exception auswerfen 
+public abstract class LevelImporter{
+/*
+	public LevelImporter(int laenge, int breite) {
+		super(laenge, breite);
+		
+	}*/
+
+	public static void main(String[] args) throws IOException{	//Fürs erste Standaloneprojekt - FileInputStream könnte bei nicht Vorhanden sein der Datei Exception auswerfen 
 			
 			Properties levelstructure = new Properties();	//Properties verwenden, um  Datei zu lesen und Levelstruktur zu importieren
 			BufferedInputStream stream = new BufferedInputStream(new FileInputStream("level_1.bml")); //.bml (BomberManLevel) mit BIS einlesen
@@ -12,17 +18,19 @@ public class LevelImporter {
 			stream.close();									//Schließen des BufferedInputStream
 			String lvlstr = levelstructure.getProperty("LEVEL"); //Lese die Levelstruktur aus der Property-Datei aus
 			readLevel(lvlstr);																	
-		
 		}
 	
 	private static void readLevel(String input) {
 	
-		for (int i = 0; i < input.length(); i++){					//Lese String ein und splitte 
-			if (input.charAt(i) == ';') System.out.print("\n");		//bei einem ';'. Ersetze 0, 1, 2
-			else if (input.charAt(i) == '1') System.out.print("W");	//gegen 'W','B',' '
-			else if (input.charAt(i) == '2') System.out.print("B"); 
-			else System.out.print(" ");
-			}
-			
+		char[][] test = new char[15][15];
+		
+		int i = 0;
+		int j = 0;
+		for (int k = 0; k < (input.length()-1); k++){				//Gesamten String einlesen und bis Länge-1 durchgehen
+			if (input.charAt(k) == ';') {j++; i = 0;}				//Bei Semikolon die erste Dimension um 1 erhöhen
+			else if (input.charAt(k) == '1') {test[j][i] = 'W';i++;}//Bei "1" neue feste Wand 
+			else if (input.charAt(k) == '2') {test[j][i] = 'B';i++;}//Bei "2" neue zerst. Wand
+			else {test[j][i] = ' '; i++;}							//Bei "0" oder allem anderem Leerzeichen einsetzen.
+		}
 	}
 }
